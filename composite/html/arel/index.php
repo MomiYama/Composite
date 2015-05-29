d8ac124427324d34256b40a7092b2b7745ee3736
MomiYama <momiji.yamamoto.4k@stu.hosei.ac.jp>
2015-05-29 12:54:01 +0900

    Create index.php

---
+++ composite/html/arel/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+/*
+Copyright © 2014 Momiji Yamamoto. All rights reserved.
+*/
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+	<title></title>
+	<script type="text/javascript" src="js/arel_beta_min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery-1.8.2.min.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/jquery.nicescroll.min.js"></script>
+    	<script type="text/javascript" src="arel_plugin_analytics_v1_2.js"></script>  
+    	<script type="text/javascript" src="logic_More1.js"></script>
+	<script type="text/javascript" charset="utf-8" src="js/main.js"></script>
+	<link rel="stylesheet" media="screen" href="style.css">
+</head>
+<body>
+
+<div id="popup" class="content-box" />
+</body>
+</html>
